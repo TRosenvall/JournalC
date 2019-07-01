6aebc719b3a0ab9d88d05c3fa68cb2464e7a7359
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-07-01 13:32:34 -0600

    Initial Commit

---
+++ JournalC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JournalC
+//
+//  Created by Timothy Rosenvall on 7/1/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
